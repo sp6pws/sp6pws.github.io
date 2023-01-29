04948fa7abda510b481012c424bf1bcfc5545f01
sp6pws <105914314+sp6pws@users.noreply.github.com>
2023-01-29 11:55:17 +0100

    Revert "WIP: Kalendarz google (#1)" (#2)
    This reverts commit 31d0bb6d013cde83bbd06f47adae380856f2cd7f.

--- devel_on_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export JEKYLL_VERSION=latest
-docker run --rm \
-  --name sp6pws.github.com \
-  --volume="$PWD:/srv/jekyll:Z" \
-  --publish-all \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll serve --watch --trace
